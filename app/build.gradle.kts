145d63f5415ac5090fcf10eac6804faec719721f
Pehan Dassanayake <pehandassanayaka@gmail.com>
2024-03-30 15:36:10 +0530

    Splash Screen Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
+import org.apache.tools.ant.util.JavaEnvUtils.VERSION_1_8
+
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,7 +39,18 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //RxJava
+    implementation ("io.reactivex.rxjava2:rxandroid:2.1.1")
+    implementation ("io.reactivex.rxjava2:rxjava:2.2.9")
+
+    //Firebase UI
+    implementation ("com.google.firebase:firebase-auth:22.3.1")
+    implementation ("com.firebaseui:firebase-ui-auth:8.0.2")
+
+
 }
